13bbb17e8df340f0022b1b604493924a55a4ffe5
Francisco Quintero Coronell <frajaquico@yahoo.es>
2021-11-10 16:00:33 -0500

    Get social profile ids

--- lib/hootsuite.rb
+++ lib/hootsuite.rb
@@ -1,6 +1,22 @@
-require "hootsuite/version"
+# frozen_string_literal: true
+
+require "dotenv/load"
+require "byebug"
+require "http"
+
+require_relative "hootsuite/version"
+require_relative "hootsuite/social_profile"
 
 module Hootsuite
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Client
+    BASE_URL = 'https://platform.hootsuite.com'
+    ACCESS_TOKEN = ENV['HOOTSUITE_ACCESS_TOKEN']
+
+    def get(endpoint)
+      HTTP.auth("Bearer #{ACCESS_TOKEN}")
+          .get("#{BASE_URL}/#{endpoint}")
+    end
+  end
 end
---
+++ lib/hootsuite/social_profile.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Hootsuite
+  class SocialProfile
+    ENDPOINT = 'v1/socialProfiles'
+
+    def initialize
+      @client = Client.new
+    end
+
+    def social_profiles
+      r = @client.get(ENDPOINT)
+
+      if r.status.success?
+        return [] if r.body.to_s.empty?
+
+        extract_ids(JSON.parse(r.body.to_s))
+      else
+        puts r.body.to_s
+      end
+    end
+
+    private
+
+    def extract_ids(response)
+      response['data'].map do |profile|
+        Profile.new(profile).id
+      end
+    end
+
+    class Profile
+      def initialize(profile)
+        @profile = profile
+      end
+
+      def id
+        @profile['id']
+      end
+
+      def type
+        @profile['type']
+      end
+    end
+  end
+end
