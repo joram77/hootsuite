bcf7c14598ab21d0edb4f85825f34b59f1d51237
Francisco Quintero Coronell <frajaquico@yahoo.es>
2021-11-10 16:03:47 -0500

    Remove useless test

--- spec/hootsuite_spec.rb
+++ spec/hootsuite_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Hootsuite do
   it "has a version number" do
     expect(Hootsuite::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
